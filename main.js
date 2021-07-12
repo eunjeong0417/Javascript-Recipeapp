f169f8327d00924c983c700b857fb14ca7bfd075
eunjeong0417 <82692118+eunjeong0417@users.noreply.github.com>
2021-07-12 13:13:25 +0900

    css 배경이미지 수정

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 const searchEl = document.querySelector('.search-result')
 const APP_ID = 'f2cf717f';
 const APP_KEY = '274f67ac048c6ca7dc21ec0c6e4f1a27';
-
-
 const formEl = document.querySelector('.header-search');
 
-//form 태그에 submit 이벤트 추가
-formEl.addEventListener('submit', (e) => {
+
+
+
+function onSubmit(e) {
     e.preventDefault();
 //e.preventDefault로 창이 새로고침되는 것을 막는다
     const query = e.target.querySelector('input').value;
@@ -14,7 +14,11 @@ formEl.addEventListener('submit', (e) => {
 
 //fetchAPI 함수의 인자로 query를 보낸다
     fetchAPI(query);
-})
+}
+
+//form 태그에 submit 이벤트 추가
+formEl.addEventListener('submit', onSubmit)
+
 
 async function fetchAPI(query) {
     const baseURL = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&to=20`
@@ -22,6 +26,7 @@ async function fetchAPI(query) {
     const data = await response.json();
     generateHTML(data.hits);
     console.log(data);
+    query = '';
 }
 
 //data.hits를 인자로 받아온다
